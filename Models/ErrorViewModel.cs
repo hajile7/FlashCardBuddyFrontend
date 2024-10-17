458a215d7188536f7cf08b683a311d12e52ec52d
Eli <elireid.28@gmail.com>
2024-10-17 17:31:58 -0400

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FlashCardBuddyFrontend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
